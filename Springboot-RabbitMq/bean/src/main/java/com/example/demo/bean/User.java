91b78ceaa250dd3038d565c650e69a404eaeaf95
gmg <minggang0829@gmail.com>
2018-06-01 17:03:41 +0800

    bean

---
+++ Springboot-RabbitMq/bean/src/main/java/com/example/demo/bean/User.java
@@ -0,0 +1,38 @@
+package com.example.demo.bean;
+
+/**
+ * @author gmg
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2018/6/1  15:49
+ */
+public class User {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ Springboot-RabbitMq/bean/src/main/java/com/example/demo/bean/UserInfo.java
@@ -0,0 +1,56 @@
+package com.example.demo.bean;
+
+/**
+ * @author gmg
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2018/6/1  15:45
+ */
+public class UserInfo {
+    private int id;
+    private String name;
+    private int age;
+    private String school;
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
