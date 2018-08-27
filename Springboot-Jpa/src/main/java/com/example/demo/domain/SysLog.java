450fcd25e3ad388c0bcd74262b2ec7d212dbb47a
gmg <minggang0829@gmail.com>
2018-08-27 10:12:31 +0800

    update

---
+++ Springboot-Jpa/src/main/java/com/example/demo/domain/SysLog.java
@@ -0,0 +1,91 @@
+package com.example.demo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author gmg
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2018/8/20  11:38
+ */
+@Entity
+@Table(name="sys_log")
+public class SysLog implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username; //用户名
+
+    private String operation; //操作
+
+    private String method; //方法名
+
+    private String params; //参数
+
+    private String ip; //ip地址
+
+    private Date createDate; //操作时间
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getParams() {
+        return params;
+    }
+
+    public void setParams(String params) {
+        this.params = params;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
